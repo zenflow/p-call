21f1b3ca3fe6027d34a500b404dc723bf1b1a7c0
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-01-15 12:11:10 -0500

    Use ES5 syntax

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-module.exports = function pCall(ctx, fn, ...args) {
-  return new Promise((resolve, reject) => {
-    fn = typeof fn === 'function' ? fn : ctx[fn]
-    fn.call(ctx, ...args, (error, result) => {
+module.exports = function pCall(ctx, fn /* , ...args */) {
+  const args = Array.prototype.slice.call(arguments, 2)
+  fn = typeof fn === 'function' ? fn : ctx[fn]
+  return new Promise(function (resolve, reject) {
+    fn.apply(ctx, args.concat(callback))
+    function callback (error, result) {
       if (error) {
         reject(error)
       } else {
         resolve(result)
       }
-    })
+    }
   })
 }
